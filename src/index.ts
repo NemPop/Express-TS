f04aaf729debd157bb69730e9ea879de0da0ed09
Nett <95707775+NemPop@users.noreply.github.com>
2022-08-31 11:42:42 +0200

    basic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there');
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['asdasfvd'] }));
+app.use(router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
